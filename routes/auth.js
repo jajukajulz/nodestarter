754761e8cdd561bc0f64a1d94958c9931f16e80c
juliank <julian.kanjere@jskdezign.com>
2019-07-31 13:29:07 +0200

    Update routes and login checks

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
 
 /* Render Login page. */
 router.get('/login',
   function(req, res){
-    res.render('login');
+    res.render('login', { title: 'User Login' });
   });
 
 /* Process Login form submission. */
+/* TODO add a user not found message */
 router.post('/login',
-  passport.authenticate('local', { failureRedirect: '/login' }),
+  passport.authenticate('local', { failureRedirect: '/auth/login' }),
   function(req, res) {
     res.redirect('/');
   });
--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,16 @@ var router = express.Router();
 /* GET users listing for an admin.
 * TODO check if user is admin before showing this page */
 router.get('/',
-    require('connect-ensure-login').ensureLoggedIn(),
+    require('connect-ensure-login').ensureLoggedIn({ redirectTo: '/auth/login' }),
     function(req, res, next) {
-      res.render('users', { user: req.user });
+      res.render('users', { user: req.user, title: 'User Config' });
       /*res.send('respond with a resource'); */
 });
 
 /* GET user profile. */
 router.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
+  require('connect-ensure-login').ensureLoggedIn({ redirectTo: '/auth/login' }),
   function(req, res){
-    res.render('profile', { user: req.user });
+    res.render('profile', { user: req.user, title: 'User Profile' });
   });
 module.exports = router;
