e2b7c21586c0e97aaa17dd95e78dbb886e72cef9
juliank <julian.kanjere@jskdezign.com>
2019-12-11 23:40:55 +0200

    Add express validation example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const {check,validationResult} = require('express-validator');//define router
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', user: req.user });
+});
+
+/* GET home page. */
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Express'});
+});
+
+/* SAMPLE FORM WITH ERRORS */
+router.post('/form', [
+  check('email', 'email is required').isEmail(),
+  check('name', 'name is required').not().isEmpty(),
+  check('password', 'password is required').not().isEmpty(),
+], function(req, res, next) {  //check validate data
+  const result= validationResult(req);
+  var errors = result.errors;  for (var key in errors) {
+        console.log(errors[key].value);
+  }
+  if (!result.isEmpty()) {
+  //response validate data to form.ejs
+     res.render('form', {
+       title: 'Express',
+      errors: errors
+    })
+  }
 });
 
 module.exports = router;
